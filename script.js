35c5ffdace281c6f8a8b89ca962f4edbe20a3318
jonasw1404-arch <jonasw1404@gmail.com>
2025-12-05 17:22:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const startBox = document.getElementById('startBox');
 const missionContainer = document.getElementById('missionContainer');
 
 startButton.addEventListener('click', () => {
-  startBox.style.display = 'none';       // Start-Rechteck ausblenden
-  missionContainer.style.display = 'flex'; // Mission-Rechtecke einblenden
+  startBox.style.display = 'none';           // Start-Rechteck ausblenden
+  missionContainer.style.display = 'flex';   // Missions-Rechtecke anzeigen
 });
