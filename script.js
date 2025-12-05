3c74087c7b64b7546d696fa3b4cb4d9c4f762c32
jonasw1404-arch <jonasw1404@gmail.com>
2025-12-05 18:26:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const startBox = document.getElementById('startBox');
 const missionContainer = document.getElementById('missionContainer');
 const raetselContainer = document.getElementById('raetselContainer');
 
-// Beispielcodes für die Rätsel
+// Richtige Codes für die Rätsel
 const correctCodes = ["1234", "5678", "9012", "3456", "7890"];
 
+// Startbutton klick
 startButton.addEventListener('click', () => {
-  startBox.style.display = 'none';           // Start-Rechteck ausblenden
-  missionContainer.style.display = 'flex';   // Missions-Rechtecke anzeigen
-  raetselContainer.style.display = 'flex';   // Rätsel anzeigen
+  startBox.style.display = 'none';
+  missionContainer.style.display = 'flex';
+  raetselContainer.style.display = 'flex';
 });
 
-// Prüfen der Rätsel
+// Prüfen der Eingaben
 const checkButtons = document.querySelectorAll('.checkButton');
 
 checkButtons.forEach((button, index) => {
