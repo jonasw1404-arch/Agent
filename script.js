60bf6d7bc028fa57c9a7295933dac373c56e5662
jonasw1404-arch <jonasw1404@gmail.com>
2025-12-05 18:55:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ startButton.addEventListener('click', () => {
   startBox.style.display = 'none';
   missionContainer.style.display = 'flex';
 });
+
+// Codes prüfen
+const buttons = document.querySelectorAll('.checkButton');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const box = button.parentElement;
+    const input = box.querySelector('.codeInput');
+    const result = box.querySelector('.result');
+    const correctCode = input.dataset.code;
+
+    if(input.value === correctCode) {
+      result.textContent = 'Richtig!';
+      result.style.color = 'lightgreen';
+    } else {
+      result.textContent = 'Falsch!';
+      result.style.color = 'red';
+    }
+  });
+});
